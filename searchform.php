2eb53c0f4265ca3cef08ea661726c43867a3adfb
Ty-Lucas Kelley <tylucaskelley@gmail.com>
2020-07-07 21:51:35 -0500

    make search text bigger

--- searchform.php
+++ searchform.php
@@ -5,7 +5,7 @@
 	class="searchform"
 	action="https://staging1.dsaneworleans.org/"
 >
-	<p>Search the website:</p>
+	<h3>Search the website:</h3>
 	<div class="search-container">
 		<input placeholder="Type here..." type="text" value="" name="s" id="s">
 		<input type="submit" id="searchsubmit" value="Search">
